8528fa29faf87e164f1f95d1c096f28d44453bef
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-03-13 15:08:22 -0700

    create cocktails page javascript file for displaying cocktail index page

---
+++ rails-with-js-project-client/js/CocktailsPage.js
@@ -0,0 +1,39 @@
+class CocktailsPage {
+
+    constructor() {
+      this.cocktials = []
+      this.formState = {
+        title: '',
+        description: '', 
+        directions: ''
+      }
+    }
+  
+    renderForm() {
+      return `
+        <form class="addCocktail">
+          <h3>Add a Cocktail Recipe</h3>
+          <p>
+            <label class="db">Title</label>
+            <input type="text" name="title" value="${this.formState.title}" />
+          </p>
+          <p>
+            <label class="db">Description</label>
+            <input type="text" name="description" value="${this.formState.description}" />
+          </p> 
+          <p>
+            <label class="db">Directions</label>
+            <input type="text" name="directions" value="${this.formState.directions}" />
+          </p>
+          <input type="submit" value="Add Cocktail Recipe" />
+        </form>
+      `
+    }
+  
+    render() {
+      return `
+        <h1>Hello from the Cocktail Recipes Page</h1>
+        ${this.renderForm()}
+      `
+    }
+  }
