56dfdcf21608e85238dabda164d5b89d18e3e933
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-03-17 14:40:28 -0700

    generated get cocktail details method in cocktailAPI class to return instance of cocktail with custom attributes

--- rails-with-js-project-client/js/CocktailAPI.js
+++ rails-with-js-project-client/js/CocktailAPI.js
@@ -1,7 +1,40 @@
 class CocktailAPI {
     static getCocktails() {
       return fetch(`${CocktailAPI.base_url}/cocktail_recipes`).then(res => res.json())
+    } 
+
+    static getCocktailShow(cocktailId) {
+      return fetch(`${CocktailAPI.base_url}/cocktail_recipes/${cocktailId}`)
+        .then(res => res.json()) 
+        .then(json => { 
+          const { 
+            data: {  
+              id,
+              attributes: {
+                title, 
+                description, 
+                directions, 
+                image_url
+              }
+            },
+            included
+          } = json 
+          return {
+            id,
+            title,
+            description, 
+            directions,
+            image_url,
+            ingredients: included.map(({id, attributes: {name, cocktail_recipe_id}}) => {
+              return {
+                id,
+                name, 
+                cocktail_recipe_id
+              }
+            })
+          }
+        
+        })
     }
-}
-  
-  CocktailAPI.base_url = "http://localhost:3000"
+  }  
+    CocktailAPI.base_url = "http://localhost:3000"
