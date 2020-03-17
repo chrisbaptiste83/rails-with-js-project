9b1801480fb2cd220a86d1167da5a68254fba870
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-03-17 14:35:55 -0700

    add clickt event listeners for link to cocktail show page and home link to index page

--- rails-with-js-project-client/js/index.js
+++ rails-with-js-project-client/js/index.js
@@ -3,10 +3,21 @@ document.addEventListener('DOMContentLoaded', () => {
     root.innerHTML = loadingGif()
     Cocktail.getAll().then(cocktails => {
      root.innerHTML = new CocktailsPage(cocktails).render()
-    }) 
-     
-  })
-  
+    })  
+document.addEventListener('click', (e) => {
+    if(e.target.matches('.cocktailShow')) {
+        let cocktail = Cocktail.findById(e.target.dataset.cocktailid)
+        cocktail.getCocktailDetails().then(cocktail => { 
+        root.innerHTML = new CocktailShowPage(cocktail).render() 
+        })  
+    }
+    if(e.target.matches('.cocktailsIndex')) {
+        root.innerHTML = new CocktailsPage(Cocktail.all).render()
+        }
+      })
+    })
+
+
   const loadingGif = () => {
     let loading = document.createElement('img')
     loading.src = 'https://i.giphy.com/media/y1ZBcOGOOtlpC/giphy.webp'
