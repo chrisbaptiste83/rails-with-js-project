278f22ca17a552b48f9590fd6c4c152610141831
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-03-18 20:12:38 -0700

    modify render method for cocktail show page

--- rails-with-js-project-client/js/CocktailShowPage.js
+++ rails-with-js-project-client/js/CocktailShowPage.js
@@ -11,21 +11,22 @@ class CocktailShowPage {
       return ul.outerHTML
     }
   
-    render() {
-      return `
-      <div class="aspect-ratio aspect-ratio--1x1">
-        <img style="background-image:url(${this.cocktail.image_url});" 
-        class="db bg-center cover aspect-ratio--object" />
-      </div>
-      <div class="ph2 ph0-ns pb3 link db">
-        <h2 class="f5 f4-ns mb0 black-90">${this.cocktail.title}</h2>
-        <h2>Description:</h2>
-        <h3 class="f6 f5 fw4 mt2 black-60">${this.cocktail.description}</h3> 
-        <h2>Directions:</h2>
-        <h3 class="f6 f5 fw4 mt2 black-60">${this.cocktail.directions}</h3>
-      </div> 
-      <h2>Ingredients:</h2>
-      ${this.renderIngredientList()}
+    render() { 
+      let article = document.createElement('article')
+      article.className = "pa2-ns center w-40-ns pv2-ns"
+      article.innerHTML = `
+        <div class="aspect-ratio aspect-ratio--1x1">
+          <img style="background-image:url(${this.cocktail.image_url});" 
+          class="br4 db bg-center cover aspect-ratio--object" />
+        </div>
+        <h3 class="f4 light-gray f1-ns mb0 black-90">${this.cocktail.title}</h3>
+        <h1>Description:</h1>
+        <h3 class="f4 light-gray f3 fw5 mt2 black-60">${this.cocktail.description}</h3> 
+        <h1>Ingredients:</h1> 
+        ${this.renderIngredientList()}
+        <h1>Directions:</h1>
+        <h3 class="f4 light-gray fw5 mt2 black-60">${this.cocktail.directions}</h3> 
       `
+      return article.outerHTML 
     }
   }
