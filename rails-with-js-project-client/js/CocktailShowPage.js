aac364128cd4549298fecba02ec6a9060616d84d
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-03-27 13:02:54 -0700

    add back button in show page, add section tags on ingredients

--- rails-with-js-project-client/js/CocktailShowPage.js
+++ rails-with-js-project-client/js/CocktailShowPage.js
@@ -15,17 +15,22 @@ class CocktailShowPage {
       let article = document.createElement('article')
       article.className = "pa2-ns center w-40-ns pv2-ns"
       article.innerHTML = `
+        <h3 class="f4 light-gray f1-ns mb0 black-90">${this.cocktail.title}</h3><br></br>
         <div class="aspect-ratio aspect-ratio--1x1">
           <img style="background-image:url(${this.cocktail.image_url});" 
           class="br4 db bg-center cover aspect-ratio--object" />
         </div>
-        <h3 class="f4 light-gray f1-ns mb0 black-90">${this.cocktail.title}</h3>
         <h1>Description:</h1>
         <h3 class="f4 light-gray f3 fw5 mt2 black-60">${this.cocktail.description}</h3> 
-        <h1>Ingredients:</h1> 
-        ${this.renderIngredientList()}
+        <h1>Ingredients:</h1>
+          <section id="ingredients"> 
+              ${this.renderIngredientList()} 
+              <button class="addIngredient">Add Ingredient</button> 
+          </section> 
+               
         <h1>Directions:</h1>
         <h3 class="f4 light-gray fw5 mt2 black-60">${this.cocktail.directions}</h3> 
+        <button onclick="javascript:history.go()">Go Back</button>
       `
       return article.outerHTML 
     }
