5fb05c795ce3ddeed07c54054235fbc174d3a0c9
Chris Baptiste <chrisbaptiste83@gmail.com>
2020-04-07 17:50:30 -0700

    delete update form function

--- rails-with-js-project-client/js/index.js
+++ rails-with-js-project-client/js/index.js
@@ -90,40 +90,6 @@ class Cocktail {
   ingredients(){ 
     return Ingredient.all.filter(ingredient => ingredient.cocktail_recipe_id == this.id)
   } 
-  
-  renderUpdateForm(){
-    return `
-          <article class="center mw8 mw9-ns hidden ba mv4">
-              <h1 class="f3 bg-near-black white mv0 pv2 ph3">Edit Cocktail:</h1>
-              <div class="pa3 bt"> 
-              <center>
-              <form class="updateCocktail"> 
-                  <p>
-                      <h2><label class="db">Title:</label></h2>
-                      <input type="text" class="db w-60" name="title" id="title" value="${this.title}"/>
-                  </p>
-                  <p>
-                      <h2><label class="db">Description:</label></h2>
-                      <input type="text" class="db w-60" name="description" id="description" value="${this.description}"/>
-                  </p> 
-                  <p>
-                      <h2><label class="db">Directions:</label></h2>
-                      <input type="text" class="db w-60" name="directions" id="directions" value="${this.directions}"/>
-                  </p> 
-                  <p>
-                      <h2><label class="db">Image URL:</label></h2>
-                      <input type="text" class="db w-60 h--80" name="image_url" id="image_url" value="${this.image_url}"/>
-                  </p>
-                      <input type="submit" value="Edit Cocktail"/></br><br>
-              </form>
-              <section id="cocktailIngredients"> 
-                  </br><button onclick = "this.disabled = true;" class="addIngredients f6 link dim br-pill ph3 pv2 mb2 dib white bg-black">Add Ingredient(s)</button>
-              </section>
-              </center>
-              </div>
-          </article>
-          `
-  }
 
   renderCard() {
     let article = document.createElement('article')
