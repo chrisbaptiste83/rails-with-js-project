5fd6c4efb09e71bc0b3b0d87a5a1c03c0d5aa31e
Christopher Michael Baptiste <chrisbaptiste83@gmail.com>
2026-03-14 12:43:48 +0000

    Add model validations and dependent: :destroy to prevent orphaned ingredients

--- rails-with-js-project-api/app/models/cocktail_recipe.rb
+++ rails-with-js-project-api/app/models/cocktail_recipe.rb
@@ -1,3 +1,5 @@
 class CocktailRecipe < ApplicationRecord 
-    has_many :ingredients
+    has_many :ingredients, dependent: :destroy
+    validates :title, presence: true
+    validates :directions, presence: true
 end
